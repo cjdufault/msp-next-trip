b566227cfaa9d12e88560f3fa1f9cb5d01fa7a9e
Connor Dufault <dufaultconnor@gmail.com>
2022-11-13 09:31:22 -0600

    init

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { NexTripLookup } from './components/NexTripLookup';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>{'Metro Transit NexTrip Dashboard'}</h1>
+        <NexTripLookup />
       </header>
     </div>
   );
---
+++ src/apiData/GetNexTrip.js
@@ -0,0 +1,18 @@
+const API_URL = 'https://svc.metrotransit.org/nextripv2';
+
+async function GetNexTrip(stopNumber) {
+
+  if (stopNumber === ''){
+    return { 
+      'success': false, 
+      'detail': 'Please enter a stop number.'
+    };
+  }
+
+  let response = await fetch(`${API_URL}/${stopNumber}`);
+  let json = await response.json();
+  json.success = response.status === 200;
+  return json;
+}
+
+export default GetNexTrip;
