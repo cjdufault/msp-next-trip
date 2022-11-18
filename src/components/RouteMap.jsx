5af160a2a14bc7ac63b1e2fb947c33afa822d4c6
Connor Dufault <dufaultconnor@gmail.com>
2022-11-18 05:43:16 -0600

    add map element

--- src/components/RouteMap.jsx
+++ src/components/RouteMap.jsx
@@ -1,11 +1,16 @@
 import Button from '@material-ui/core/Button';
+import { MapContainer, TileLayer } from 'react-leaflet'
 import GetRouteMapData from "../apiData/GetRouteMapData";
 
 export const RouteMap = ({ routeNumber, mapExitCallback }) => {
   return (
     <div>
       <Button onClick={mapExitCallback}>X</Button>
-      
+      <MapContainer className={'route-map-container'} center={[44.987, -93.258]} zoom={13} >
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      /></MapContainer>
     </div>
   )
 };
