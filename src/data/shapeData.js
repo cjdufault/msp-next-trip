845ef070d7c97ec3812628720c1feae5d48fefd5
Connor Dufault <dufaultconnor@gmail.com>
2022-12-03 06:51:53 -0600

    update shapeData.js to read route shapes for both directions

--- src/data/shapeData.js
+++ src/data/shapeData.js
@@ -1,7 +1,43 @@
-export async function getRouteShape(routeNumber) {
-  let response = await fetch(`/shapes/${routeNumber}.json`);
-  let routeData = await response.json();
+import constants from '../constants.json'
 
-  let shapes = routeData.Points.map((point) => [point.Lat, point.Lon]);
-  return shapes;
+export async function getRouteShape(routeNumber) {
+  let directions = await getDirectionsForRoute(routeNumber);
+
+  let shapesPerDirection = directions.map(async (direction) => {
+    let response = await fetch(`/shapes/${routeNumber}_${direction}.json`);
+    let routeData = await response.json();
+    return routeData.Points.map((point) => [point.Lat, point.Lon]);
+  });
+
+  // hardcoding array indexes here b/c I suck at async
+  return [].concat(await shapesPerDirection[0], await shapesPerDirection[1]);
+}
+
+async function getDirectionsForRoute(routeNumber) {
+  let response = await fetch(`${constants.API_URL}/directions/${routeNumber}`);
+  let data = await response.json();
+
+  let directions = data.map((direction) => {
+    let directionCode;
+    switch(direction.direction_name) {
+      case ('Northbound'):
+        directionCode = 'NB';
+        break;
+      case ('Southbound'):
+        directionCode = 'SB';
+        break;
+      case ('Eastbound'):
+        directionCode = 'EB';
+        break;
+      case ('Westbound'):
+        directionCode = 'WB';
+        break;
+      default:
+        console.error(`Recieved invalid direction name" "${direction.direction_name}"`)
+        break;
+    }
+    return directionCode;
+  })
+
+  return directions;
 }
